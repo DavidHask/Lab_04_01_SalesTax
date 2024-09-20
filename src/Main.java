1586d0599df20adc53b54a559cd5cfcfd0f2cac0
DavidHask <haskeldd@mail.uc.edu>
2024-09-20 11:32:20 -0400

    Lab 4, first task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double itemPrice = 42;
+        double SALES_TAX = 0.05;
+        double tax = 0;
+        double totalPrice = 0;
+
+        tax = itemPrice * SALES_TAX;
+        totalPrice = itemPrice + tax;
+
+        System.out.println("\nThe item cost " + itemPrice + ", at 5% sales tax, " +
+                "the tax is " + tax + ", bringing the total to " + totalPrice);
     }
 }
